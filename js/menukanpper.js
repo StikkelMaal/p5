82c93366049403f1a2d90f17d5866b5461ef710d
Artaey <95622783+Artaey@users.noreply.github.com>
2022-04-22 11:14:36 +0200

    jb

---
+++ js/menukanpper.js
@@ -0,0 +1,20 @@
+let pages = ["index.html", "købbillet.html", "støtos.html", "omos.html", "frivilige.html", "sponsorer.html"]
+let topage = 3;
+
+function knap(hvor){
+    let links = document.getElementsByClassName("nav-link");
+    for(let i = 0; i < pages.length; i++){
+        if(i == hvor){
+            topage = i;
+            if(i !== 0){
+                links[i-1].style.color = "red";
+            }
+        }else{
+        }
+    }
+    setTimeout(turnpage, 250);
+}
+
+function turnpage() {
+    window.location.href = pages[topage]
+}

9f3cd40f091662a971d82ed707e0a77204d1fa68
Anna Østergaard Jensen <95587296+Luxvinylcrafts@users.noreply.github.com>
2022-04-23 16:58:41 +0200

    frivillig galleri færdig

--- frivilligegalleri.js
+++ frivilligegalleri.js
@@ -1,15 +1,13 @@
 var indexValue = 0;
-setTimeout(sildeShow, 2500);
-function sildeShow(){
-    var x;
-    const img = document.querySelectorAll("img");
-    for(x = 0; x < img.length; x++){
-    img[x].style.display = "none"; 
-
-    }
-    indexValue++;
-    if(indexValue > img.length){indexValuev = 1}
-    img[indexValue -1].style.display = "block"; 
-
-}
-sildeShow();
+      function slideShow(){
+        setTimeout(slideShow, 2500);
+        var x;
+        const img = document.querySelectorAll("img");
+        for(x = 0; x < img.length; x++){
+          img[x].style.display = "none";
+        }
+        indexValue++;
+        if(indexValue > img.length){indexValue = 1}
+        img[indexValue -1].style.display = "block";
+      }
+      slideShow();

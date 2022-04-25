7e52be68f4925f0ef8fe7ba758e186f955c44dac
Artaey <95622783+Artaey@users.noreply.github.com>
2022-04-25 09:22:54 +0200

    k,,

--- js/menukanpper.js
+++ js/menukanpper.js
@@ -1,7 +1,7 @@
 //lynges kode
 
 let pages = ["index.html", "købbillet.html", "index.html", "støtos.html", "omos.html", "frivillige.html", "sponsor.html"]
-let topage = 3;
+let topage;
 
 function knap(hvor){
     let links = document.getElementsByClassName("nav-link");
